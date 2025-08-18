4d6ffa0265480d985d1a1e6ff381ae827bfa701a
Juan Diego Munoz Ospina <jdmunozo_1@uqvirtual.edu.co>
2025-08-18 17:11:45 -0500

    refactorizando

--- juandi/api-gateway/src/main/java/com/apigateway/config/GatewayConfig.java
+++ juandi/api-gateway/src/main/java/com/apigateway/config/GatewayConfig.java
@@ -1,6 +1,7 @@
 package com.apigateway.config;
 
 import com.apigateway.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,33 @@ public class GatewayConfig {
 
     private final JwtAuthenticationFilter filter;
 
+    @Value("${services.uris.bank-service}")
+    private String bankServiceUri;
+
+    @Value("${services.uris.account-service}")
+    private String accountServiceUri;
+
+    @Value("${services.uris.security-service}")
+    private String securityServiceUri;
+
+    @Value("${routes.bank-service.id}")
+    private String bankServiceId;
+
+    @Value("${routes.bank-service.path}")
+    private String bankServicePath;
+
+    @Value("${routes.account-service.id}")
+    private String accountServiceId;
+
+    @Value("${routes.account-service.path}")
+    private String accountServicePath;
+
+    @Value("${routes.security-service.id}")
+    private String securityServiceId;
+
+    @Value("${routes.security-service.path}")
+    private String securityServicePath;
+
     public GatewayConfig(JwtAuthenticationFilter filter) {
         this.filter = filter;
     }
@@ -18,13 +46,14 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("bank_service_route", r -> r.path("/api/v1/banks/**")
+                .route(bankServiceId, r -> r.path(bankServicePath)
                         .filters(gtf -> gtf.filter(filter))
-                        .uri("http://localhost:8081"))
-                .route("account_service_route", r -> r.path("/api/v1/accounts/**")
+                        .uri(bankServiceUri))
+                .route(accountServiceId, r -> r.path(accountServicePath)
                         .filters(gtf -> gtf.filter(filter))
-                        .uri("http://localhost:8082"))
-                .route("auth-service", route -> route.path("/api/auth/**").uri("http://localhost:8086"))
+                        .uri(accountServiceUri))
+                .route(securityServiceId, route -> route.path(securityServicePath)
+                        .uri(securityServiceUri))
                 .build();
     }
 }
